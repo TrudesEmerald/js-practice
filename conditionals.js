809e35c793add1df11a8f06a2683546935fdcf6a
Getrude Kyei Baffour <getrudekyeikbh@gmail.com>
2021-10-13 10:51:47 +0000

    first

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+function addFigures(fig1, fig2, fig3 ){
+let addFig= fig1 + fig2 + fig3
+console.log(addFig);
+ }
+
+addFigures(20, 30, 40);
+
+
+function addThreeNumbers(num1, num2, num3){
+	let total=num1 + num2 + num3 + ' students in total'
+	return total
+}
+
+let inTotal = addThreeNumbers(30, 20, 10)
+console.log(inTotal)
